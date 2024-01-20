2fcce568a46212df6b8f645c92aba6ea14c77a14
robelbruk <robelbruk5@gmail.com>
2024-01-19 23:49:09 -0500

    This is Assignment #1.

---
+++ LSP_Project_Assignment1_Spring_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,13 @@
+/**
+ * Name: Robel Bruk
+ */
+
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");
+	}
+
+}
