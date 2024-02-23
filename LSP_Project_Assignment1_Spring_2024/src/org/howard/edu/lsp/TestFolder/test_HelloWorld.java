a83b56f6a660f6ca09c31625d908558695c44ac5
robelbruk <robelbruk5@gmail.com>
2024-02-23 13:41:57 -0500

    test commit @ vscode

---
+++ LSP_Project_Assignment1_Spring_2024/src/org/howard/edu/lsp/TestFolder/test_HelloWorld.java
@@ -0,0 +1,10 @@
+package org.howard.edu.lsp.TestFolder;
+
+public class test_HelloWorld {
+    /* This is my test java program
+     * This will print 'Hello, World!' as the output
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello, World!"); // print Hello, World
+    } 
+}
